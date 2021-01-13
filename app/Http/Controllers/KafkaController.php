0ecbde735ea6d12f267c8d40b9490086a91bacd9
Emanuel Riolan <emanuelriolan@gmail.com>
2021-01-13 15:54:39 +0700

    Consumer Eharvesting data SAP validation Jumlah

--- app/Http/Controllers/KafkaController.php
+++ app/Http/Controllers/KafkaController.php
@@ -184,13 +184,13 @@ class KafkaController extends Controller {
 				{
 					if($field!='POST_STATUS' && $field!='POST_TIMESTAMP')
 					{
+						if($field=='JUMLAH')
+						{
+							$value = $value==null?0:$value;
+						}
 						$insert_into .= $insert_into==''?$field:','.$field;
 						$insert_value .= $insert_value==''?"'".$value."'":",'".$value."'";
 						if($topic=='HRV_MSA_PROCESS_T_STATUS_TO_SAP_DENDA_PANEN'){
-							if($field=='JUMLAH')
-							{
-								$value = $value==null?0:$value;
-							}
 							if($field=='COMP_CODE' || $field=='PROFILE_NAME' || $field=='NO_BCC' || $field=='KODE_DENDA_PANEN'){
 								$where .= $where==''?$field."='".$value."'":" AND ".$field."='".$value."'";
 							}else {
