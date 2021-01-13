e5fcc7e944744ba07ccc9f2a2a941ac79f11e856
Emanuel Riolan <emanuelriolan@gmail.com>
2021-01-13 15:25:55 +0700

    Consumer Eharvesting data SAP

--- app/Http/Controllers/KafkaController.php
+++ app/Http/Controllers/KafkaController.php
@@ -35,13 +35,7 @@ class KafkaController extends Controller {
 		// Kafka Config
 		$conf = new RdKafka\Conf();
 		$conf->set( 'group.id', 'MSA_INTERNAL_GROUP' );
-		// $conf->set('security.protocol', 'sasl_plaintext');//sasl_plaintext SASL_SSL
-		// $conf->set('sasl.mechanisms', 'PLAIN');
-		// $conf->set('sasl.username', 'admin' );
-		// $conf->set('sasl.password', '12345' );
 		$Kafka = new RdKafka\Consumer( $conf );
-
-		//$Kafka->addBrokers( config('app.kafkahost') );
 		$Kafka->addBrokers( env('KAFKA_BROKER') );
 
 		$topicConf = new RdKafka\TopicConf();
@@ -62,7 +56,6 @@ class KafkaController extends Controller {
 			} 
 			else {
 				$payload = json_decode( $message->payload, true );
-				// print $message->payload.PHP_EOL;
 				$last_offset = $this->cek_offset_payload( $topic );
 				if ( $last_offset !== false ) {
 					if ( $last_offset ==null) {
@@ -93,8 +86,6 @@ class KafkaController extends Controller {
 		" );
 		$this->eharvesting_oracle->commit();
 
-		// dd($payload);
-		// $table = $table.'_TEST';
 		try {
 			if(ISSET($payload['ID']))
 			{
@@ -116,7 +107,7 @@ class KafkaController extends Controller {
 						END;";
 				$this->eharvesting_oracle->statement($sql);
 				$this->eharvesting_oracle->commit();
-				return date( 'Y-m-d H:i:s' )." - $topic - INSERT ".$payload['ID'].' - SUCCESS '.PHP_EOL;
+				// return date( 'Y-m-d H:i:s' )." - $topic - INSERT ".$payload['ID'].' - SUCCESS '.PHP_EOL;
 			}
 			else 
 			{
@@ -134,6 +125,115 @@ class KafkaController extends Controller {
 
 	# PHP Kafka HRV_MSA_PROCESS_SAP
 	public function HRV_MSA_PROCESS_T_STATUS_TO_SAP($topic){
+		// Kafka Config
+		$conf = new RdKafka\Conf();
+		$conf->set( 'group.id', 'MSA_INTERNAL_GROUP' );
+		$Kafka = new RdKafka\Consumer( $conf );
+		$Kafka->addBrokers( env('KAFKA_BROKER') );
+		$topicConf = new RdKafka\TopicConf();
+		$topicConf->set( 'auto.commit.interval.ms', 100 );
+		$topicConf->set( 'auto.offset.reset', 'smallest' );
 
+		$Topic = $Kafka->newTopic( $topic, $topicConf );
+		$Topic->consumeStart( 0, RD_KAFKA_OFFSET_BEGINNING );
+
+		while ( true ) {
+			$message = $Topic->consume( 0, 1000 );
+			if ( null === $message ) {
+				continue;
+			} 
+			else if ( $message->err ) {
+				echo $message->errstr(), "\n";
+				break;
+			} 
+			else {
+				$payload = json_decode( $message->payload, true );
+				$last_offset = $this->cek_offset_payload( $topic );
+				if ( $last_offset !== false ) {
+					if ( $last_offset ==null) {
+						if( (int)$message->offset >= $last_offset ){
+							echo $this->HRV_MSA_PROCESS_T_STATUS_TO_SAP_STATEMENT( $payload, (int)$message->offset, $topic, str_replace('HRV_MSA_PROCESS_', '', $topic) );
+						}	
+					} else {
+						if ( (int)$message->offset > $last_offset ){
+							echo $this->HRV_MSA_PROCESS_T_STATUS_TO_SAP_STATEMENT( $payload, (int)$message->offset, $topic, str_replace('HRV_MSA_PROCESS_', '', $topic) );
+						}	
+					}
+				}
+			}
+		}
+	}
+
+	# PHP Query HRV_MSA_PROCESS_TRANSACTION_STATEMENT
+	public function HRV_MSA_PROCESS_T_STATUS_TO_SAP_STATEMENT( $payload, $offset, $topic, $table ) {
+		//update offset payloads
+		$this->eharvesting_oracle->statement( "
+			UPDATE 
+				EHARVESTING.TM_KAFKA_PAYLOADS
+			SET
+				OFFSET = $offset,
+				EXECUTE_DATE = SYSDATE
+			WHERE
+				TOPIC_NAME = '$topic'
+		" );
+		$this->eharvesting_oracle->commit();
+
+		try {
+				$insert_into = ''; $insert_value = ''; $update_set = ''; $where = '';
+				foreach ($payload as $field => $value) 
+				{
+					if($field!='POST_STATUS' && $field!='POST_TIMESTAMP')
+					{
+						$insert_into .= $insert_into==''?$field:','.$field;
+						$insert_value .= $insert_value==''?"'".$value."'":",'".$value."'";
+						if($topic=='HRV_MSA_PROCESS_T_STATUS_TO_SAP_DENDA_PANEN'){
+							if($field=='COMP_CODE' || $field=='PROFILE_NAME' || $field=='NO_BCC' || $field=='KODE_DENDA_PANEN'){
+								$where .= $where==''?$field."='".$value."'":" AND ".$field."='".$value."'";
+							}else {
+								$update_set .= $update_set==''?$field."='".$value."'":",".$field."='".$value."'";
+							}
+						}
+						if($topic=='HRV_MSA_PROCESS_T_STATUS_TO_SAP_NAB'){
+							if($field=='COMP_CODE' || $field=='PROFILE_NAME' || $field=='ESTATE_CODE' || $field=='NO_NAB' || $field=='NO_BCC' || $field=='TANGGAL'){
+								$where .= $where==''?$field."='".$value."'":" AND ".$field."='".$value."'";
+							}else {
+								$update_set .= $update_set==''?$field."='".$value."'":",".$field."='".$value."'";
+							}
+						}
+						if($topic=='HRV_MSA_PROCESS_T_STATUS_TO_SAP_EBCC'){
+							if($field=='COMP_CODE' || $field=='PROFILE_NAME' || $field=='NO_BCC'){
+								$where .= $where==''?$field."='".$value."'":" AND ".$field."='".$value."'";
+							}else {
+								$update_set .= $update_set==''?$field."='".$value."'":",".$field."='".$value."'";
+							}
+						}
+					}
+				}
+				$sql = "
+						DECLARE
+							n_count number;
+						BEGIN
+							SELECT count(*) into n_count 
+							FROM EHARVESTING.$table
+							WHERE $where;
+							IF n_count > 0 THEN
+								UPDATE EHARVESTING.$table
+								SET $update_set
+								WHERE $where;
+							ELSE
+								INSERT INTO EHARVESTING.$table ($insert_into) 
+								VALUES ($insert_value);
+							END IF;
+						END;";
+				$this->eharvesting_oracle->statement($sql);
+				$this->eharvesting_oracle->commit();
+				// return date( 'Y-m-d H:i:s' )." - $topic - INSERT ".$payload['ID'].' - SUCCESS '.PHP_EOL;
+		}
+		catch ( \Throwable $e ) {
+			return date( 'Y-m-d H:i:s' )." - $topic - INSERT ".$payload['ID'].' - FAILED '.$e->getMessage().PHP_EOL;
+		}
+		catch ( \Exception $e ) {
+			return date( 'Y-m-d H:i:s' )." - $topic - INSERT ".$payload['ID'].' - FAILED '.$e->getMessage().PHP_EOL;
+		}
 	}
 }
